2c502716b52b958bc8fabf5fd6cd4ee8b3513502
dinhnhxd <dinh.nguyen@fdssoft.com>
2022-08-28 00:07:41 +0700

    remove header

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Browser } from '@capacitor/browser';
+import { Router } from '@angular/router';
+import { InAppBrowser, InAppBrowserOptions } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +9,34 @@ import { Browser } from '@capacitor/browser';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  options: InAppBrowserOptions = {
+    fullscreen: 'yes',
+    zoom: 'no',
+    location: 'yes',
+    toolbar: 'yes',
+    hideurlbar: 'yes',
+    closebuttoncaption: 'Đóng',
+    hidenavigationbuttons: 'yes',
 
-  constructor() {}
+  };
+  constructor(
+    private router: Router,
+    private inAppBrowser: InAppBrowser,
+  ) {
+    // Browser.addListener('browserFinished', ()=>{
+    //   console.log('browserFinished')
+    //   this.router.navigateByUrl('splash')
+    // })
+  }
   ngOnInit() {
     this.openBrowser()
   }
-  async openBrowser(){
-    console.log('onclick');
-    await Browser.open({ url: 'http://erashop.vn/' });
-    Browser.addListener('browserFinished', ()=>{
-      console.log('browserFinished')
+  async openBrowser() {
+
+    this.inAppBrowser.create('http://erashop.vn/', '_self', {
+      location: 'no', hideurlbar: 'yes',
+      closebuttoncaption: 'Đóng',
+      hidenavigationbuttons: 'yes',
     })
   }
 }
